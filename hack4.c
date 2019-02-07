2bc5c4087b8374a84d94d65086b23d4f9cd8e074
Buraq <benilamburaq@gmail.com>
2019-02-06 20:03:31 -0600

    Create hack4.c

---
+++ hack4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+int main() {
+
+
+double annualRateOfReturn, annualRateOfInflation;
+int IniBalance, ;
+
+
+
+
+
+
+
+
+
+  return 0 ;
+
+}
